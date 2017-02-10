655cc0f6ed5a8cf8a668bc88b797d3d1c9f5dec9
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-02-10 16:40:43 +0100

    Added Ropsten network to deploy to.

--- truffle/truffle.js
+++ truffle/truffle.js
@@ -1,6 +1,11 @@
 module.exports = {
-  rpc: {
-    host: "localhost",
-    port: 8545
-  }
+    rpc: {
+        host: "localhost",
+        port: 8545
+    },
+    networks: {
+        ropsten: {
+            network_id: 3
+        }
+    }
 };
