e79055ede75b73493df5c4e37be3acbf366c65bf
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-02-10 16:11:34 +0100

    Tightened tests on WithConfirmation.

--- truffle/contracts/WithConfirmation.sol
+++ truffle/contracts/WithConfirmation.sol
@@ -2,16 +2,24 @@ pragma solidity ^0.4.5;
 
 contract WithConfirmation {
     uint requiredCount;
+
     struct Confirmation {
         uint count;
         mapping (address => bool) confirmed;
     }
+
     mapping (bytes32 => Confirmation) public confirmations;
 
     event OnConfirmationRequired(bytes32 indexed key);
-    
+
     function WithConfirmation(uint _requiredCount) {
         requiredCount = _requiredCount;
+    }
+
+    function getConfirmationOf(bytes32 key, address user)
+        constant
+        returns (bool confirmed) {
+        return confirmations[key].confirmed[user];
     }
 
     function calculateKey(bytes data)
@@ -20,18 +28,40 @@ contract WithConfirmation {
         return sha3(data);
     }
 
+    function confirm(bytes32 key)
+        private
+        returns (uint count) {
+        if (confirmations[key].confirmed[msg.sender]) {
+            throw;
+        }
+        confirmations[key].confirmed[msg.sender] = true;
+        confirmations[key].count++;
+        return confirmations[key].count;
+    }
+
+    function unconfirm(bytes32 key)
+        internal
+        returns (uint count) {
+        if (!confirmations[key].confirmed[msg.sender]) {
+            throw;
+        }
+        confirmations[key].confirmed[msg.sender] = false;
+        confirmations[key].count--;
+        return confirmations[key].count;
+    }
+
     modifier isConfirmed {
         bytes32 key = calculateKey(msg.data);
-        if (!confirmations[key].confirmed[msg.sender]) {
-            confirmations[key].confirmed[msg.sender] = true;
-            confirmations[key].count++;
-        }
-        if (confirmations[key].count < requiredCount) {
-            OnConfirmationRequired(key);
+        uint count = confirm(key);
+        if (count < requiredCount) {
+            if (count == 1) {
+                // We notify only on first request
+                OnConfirmationRequired(key);
+            }
             // Do not enter into the rest of the function
             return;
         }
-        delete confirmations[key];
+        // TODO improvement? delete confirmations[key];
         // Enter the rest of the function now
         _;
     }
--- truffle/test/withConfirmation.js
+++ truffle/test/withConfirmation.js
@@ -6,9 +6,10 @@ contract('Registry', function(accounts) {
     var user1, user2;
 
     before("should prepare accounts", function() {
-        assert.isAtLeast(accounts.length, 2, "should have at least 2 accounts");
+        assert.isAtLeast(accounts.length, 3, "should have at least 3 accounts");
         user1 = accounts[0];
         user2 = accounts[1];
+        user3 = accounts[2];
         return Extensions.makeSureAreUnlocked(
                 [ user1, user2 ])
             .then(function() {
@@ -42,10 +43,18 @@ contract('Registry', function(accounts) {
                         .formatter(receiptAndKey[0].logs[0]);
                     var key = receiptAndKey[1];
                     assert.strictEqual(receivedEvent.args.key, key, "should be the key");
-                    return instance.info();
+                    return Promise.all([
+                             instance.info(),
+                             instance.confirmations(key),
+                             instance.getConfirmationOf(key, user1),
+                             instance.getConfirmationOf(key, user2)
+                        ]);
                 })
-                .then(info => {
-                    assert.strictEqual(web3.toUtf8(info), "", "should not be set");
+                .then(infos => {
+                    assert.strictEqual(web3.toUtf8(infos[0]), "", "should not be set");
+                    assert.strictEqual(infos[1].toNumber(), 1, "should have 1 confirmed count");
+                    assert.isTrue(infos[2], "should be confirmed");
+                    assert.isFalse(infos[3], "should not be confirmed");
                 });
         });
 
@@ -53,7 +62,7 @@ contract('Registry', function(accounts) {
 
     describe("when already 1 pending action", function() {
 
-        var instance;
+        var instance, key1;
 
         beforeEach("should create a brand new withConfirmation with 1 action pending", function() {
             return WithConfirmationMock.new(2, { from: user1 })
@@ -61,7 +70,9 @@ contract('Registry', function(accounts) {
                     instance = created;
                     return instance.setInfo("hello1");
                 })
-                .then(txHash => web3.eth.getTransactionReceiptMined(txHash));
+                .then(txHash => web3.eth.getTransactionReceiptMined(txHash))
+                .then(receipt => instance.calculateKey(instance.contract.setInfo.getData("hello1")))
+                .then(key => { key1 = key; });
         });
 
         it("should be able to create an independent pending confirmation", function() {
@@ -76,18 +87,58 @@ contract('Registry', function(accounts) {
                         .formatter(receiptAndKey[0].logs[0]);
                     var key = receiptAndKey[1];
                     assert.strictEqual(receivedEvent.args.key, key, "should be the key");
-                    return instance.info();
+                    return Promise.all([
+                            instance.info(),
+                            instance.confirmations(key),
+                            instance.getConfirmationOf(key, user1),
+                            instance.getConfirmationOf(key, user2),
+                            instance.confirmations(key1),
+                            instance.getConfirmationOf(key1, user1),
+                            instance.getConfirmationOf(key1, user2)
+                        ]);
                 })
-                .then(info => {
-                    assert.strictEqual(web3.toUtf8(info), "", "should not be set");
+                .then(infos => {
+                    assert.strictEqual(web3.toUtf8(infos[0]), "", "should not be set");
+                    assert.strictEqual(infos[1].toNumber(), 1, "should be 1 confirmation for hello2");
+                    assert.isTrue(infos[2], "should mark user1 as confirmed on hello2");
+                    assert.isFalse(infos[3], "should mark user2 as not confirmed on hello2");
+                    assert.strictEqual(infos[4].toNumber(), 1, "should be 1 confirmation for hello1");
+                    assert.isTrue(infos[5], "should mark user1 as confirmed on hello1");
+                    assert.isFalse(infos[6], "should mark user2 as not confirmed on hello1");
                 });
+        });
+
+        it("should be possible to finally set the value", function() {
+            return instance.setInfo("hello1", { from: user2 })
+                .then(txHash => web3.eth.getTransactionReceiptMined(txHash))
+                .then(receipt => {
+                    assert.strictEqual(receipt.logs.length, 0, "should have no event");
+                    return Promise.all([
+                            instance.info(),
+                            instance.confirmations(key1),
+                            instance.getConfirmationOf(key1, user1),
+                            instance.getConfirmationOf(key1, user2)
+                        ]);
+                })
+                .then(infos => {
+                    assert.strictEqual(web3.toUtf8(infos[0]), "hello1", "should have been set");
+                    assert.strictEqual(infos[1].toNumber(), 2, "should have 2 confirmations");
+                    assert.isTrue(infos[2], "should have marked user1 as confirmed");
+                    assert.isTrue(infos[3], "should have marked user2 as confirmed");
+                });
+        });
+
+        it("should fail if same user confirm same thing again", function() {
+            return Extensions.expectedExceptionPromise(
+                () => instance.setInfo("hello1", { from: user1, gas: 3000000 }),
+                3000000);
         });
 
     });
 
     describe("when already 2 pending actions", function() {
 
-        var instance;
+        var instance, key1, key2;
 
         beforeEach("should create a brand new withConfirmation with 2 actions pending", function() {
             return WithConfirmationMock.new(2)
@@ -98,7 +149,15 @@ contract('Registry', function(accounts) {
                             instance.setInfo("hello2", { from: user1 })
                         ]);
                 })
-                .then(txHashes => web3.eth.getTransactionReceiptMined(txHashes));
+                .then(txHashes => web3.eth.getTransactionReceiptMined(txHashes))
+                .then(receipt => Promise.all([
+                        instance.calculateKey(instance.contract.setInfo.getData("hello1")),
+                        instance.calculateKey(instance.contract.setInfo.getData("hello2"))
+                    ]))
+                .then(keys => {
+                    key1 = keys[0];
+                    key2 = keys[1];
+                });
         });
 
         it("should be hello1 if it is sent first", function() {
@@ -127,9 +186,9 @@ contract('Registry', function(accounts) {
 
     });
 
-    describe("when already 2 pending actions, will overwrite", function() {
+    describe("when already 2 pending actions, will overwrite each other", function() {
 
-        var instance;
+        var instance, key1, key2;
 
         beforeEach("should create a brand new withConfirmation with 2 actions pending", function() {
             return WithConfirmationMock.new(2)
@@ -140,7 +199,15 @@ contract('Registry', function(accounts) {
                             instance.setInfo("hello2", { from: user1 })
                         ]);
                 })
-                .then(txHashes => web3.eth.getTransactionReceiptMined(txHashes));
+                .then(txHashes => web3.eth.getTransactionReceiptMined(txHashes))
+                .then(receipt => Promise.all([
+                        instance.calculateKey(instance.contract.setInfo.getData("hello1")),
+                        instance.calculateKey(instance.contract.setInfo.getData("hello2"))
+                    ]))
+                .then(keys => {
+                    key1 = keys[0];
+                    key2 = keys[1];
+                });
         });
 
         it("should be hello1 if it is sent first, overwritten by hello2", function() {
@@ -168,6 +235,71 @@ contract('Registry', function(accounts) {
                 })
                 .then(info => {
                     assert.strictEqual(web3.toUtf8(info), "hello1", "should have now be set");
+                });
+        });
+
+    });
+
+    describe("when needs 3 confirmations", function() {
+
+        var instance, key1;
+
+        beforeEach("should create a brand new withConfirmation with 1 action pending with 1 confirmation", function() {
+            return WithConfirmationMock.new(3)
+                .then(created => {
+                    instance = created;
+                    return instance.setInfo("hello1", { from: user1 });
+                })
+                .then(txHash => web3.eth.getTransactionReceiptMined(txHash))
+                .then(receipt => instance.calculateKey(instance.contract.setInfo.getData("hello1")))
+                .then(key => { key1 = key; });
+        });
+
+        it("should not set and not notify when 2nd confirmation", function() {
+            return instance.setInfo("hello1", { from: user2 })
+                .then(txHash => web3.eth.getTransactionReceiptMined(txHash))
+                .then(receipt => {
+                    assert.strictEqual(receipt.logs.length, 0, "should have no event");
+                    return Promise.all([
+                            instance.info(),
+                            instance.confirmations(key1),
+                            instance.getConfirmationOf(key1, user1),
+                            instance.getConfirmationOf(key1, user2),
+                            instance.getConfirmationOf(key1, user3)
+                        ]);
+                })
+                .then(infos => {
+                    assert.strictEqual(web3.toUtf8(infos[0]), "", "should have now be set");
+                    assert.strictEqual(infos[1].toNumber(), 2, "should have counted 2");
+                    assert.isTrue(infos[2], "should be marked user1 as confirmed");
+                    assert.isTrue(infos[3], "should be marked user2 as confirmed");
+                    assert.isFalse(infos[4], "should be marked user3 as not confirmed");
+                })
+        });
+
+        it("should be set when sent 3rd confirmation", function() {
+            return Promise.all([
+                    instance.setInfo("hello1", { from: user2 }),
+                    instance.setInfo("hello1", { from: user3 })
+                ])
+                .then(txHashes => web3.eth.getTransactionReceiptMined(txHashes))
+                .then(receipts => {
+                    assert.strictEqual(receipts[0].logs.length, 0, "should have no event");
+                    assert.strictEqual(receipts[1].logs.length, 0, "should have no event");
+                    return Promise.all([
+                            instance.info(),
+                            instance.confirmations(key1),
+                            instance.getConfirmationOf(key1, user1),
+                            instance.getConfirmationOf(key1, user2),
+                            instance.getConfirmationOf(key1, user3)
+                        ]);
+                })
+                .then(infos => {
+                    assert.strictEqual(web3.toUtf8(infos[0]), "hello1", "should have now be set");
+                    assert.strictEqual(infos[1].toNumber(), 3, "should have counted 3");
+                    assert.isTrue(infos[2], "should be marked user1 as confirmed");
+                    assert.isTrue(infos[3], "should be marked user2 as confirmed");
+                    assert.isTrue(infos[4], "should be marked user3 as confirmed");
                 });
         });
 
