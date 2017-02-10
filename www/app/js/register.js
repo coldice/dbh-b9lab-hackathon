39ecee71026e31c09aa0cc96971da08b4caef8d6
Ibo Sy <ibo@b9lab.com>
2017-02-10 16:58:14 +0100

    add register html js, update names

---
+++ www/app/js/register.js
@@ -0,0 +1,8 @@
+function loadActions() {
+    $("#btn_submit_register").click(function() {
+        var n_name = $("#txt_name").val();
+        var n_location = $("#txt_location").val();
+    }
+}
+
+//lbl_account
