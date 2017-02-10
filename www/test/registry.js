5406dcb6ffba07caa648624bfefdfc941ffed556
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-02-10 18:12:42 +0100

    Fixed package.json. Try test with spy.

--- www/test/registry.js
+++ www/test/registry.js
@@ -1,7 +1,31 @@
-const Web3 = require('web3');
+const chai = require('chai');
+const spies = require('chai-spies');
+chai.use(spies);
+var expect = chai.expect;
 
-const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+const web3 = {
+	net: {
+		getVersion: function() {
+			return new Promise(function() {}
+				)
+		}
+	},
+	currentProvider: {}
+};
+const Registry = {
+	setProvider: function() {},
+	setNetwork: function() {}
+};
 
-const compiled = require('../../truffle/build/contracts/Registry.sol.js');
-const script = require('../app/js/registry.js');
+require('../app/js/registry.js');
 
+describe("basic calls", function() {
+	console.log(registry);
+	it("prepare called as expected", function() {
+		// console.log(Registry);
+		var spiedWeb3GetVersion = chai.spy(web3.net.getVersion);
+		expect(spiedWeb3GetVersion).to.be.spy;
+		registry.prepare(web3, Registry);
+		expect(spiedWeb3GetVersion).to.have.been.called();
+	});
+});
