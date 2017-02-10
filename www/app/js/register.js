d9042e1a15c8cea6bc2cfe83f5c8bb302a7b1b57
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-02-10 17:25:58 +0100

    Some changes to the registry UI.

--- www/app/js/register.js
+++ www/app/js/register.js
@@ -1,12 +1,69 @@
+function isNameTaken() {
+    return web3.eth.getFirstAccountPromise()
+        .catch(error => {
+            console.error(error);
+            // Show the UI that there is no account
+        })
+        .then(account => {
+            return registry.getAddressOf(n_name)
+                .catch(error => {
+                    console.error(error);
+                    // Show the UI that there is a problem connecting the contract.
+                })
+                .then(whoElse => {
+                    if (whoElse != account) {
+                        // Show the UI that the name is already taken.
+                    } else if (whoElse == account) {
+                        // Show the UI that the name is already set.
+                    } else {
+                        // That's fine should be possible to set it.
+                    }
+                });
+        });
+}
+
 function loadActions() {
     $("#btn_submit_register").click(function() {
         var n_name = $("#txt_name").val();
         var n_location = $("#txt_location").val();
+
+    return web3.eth.getFirstAccountPromise()
+        .catch(error => {
+            console.error(error);
+            // Show the UI that there is no account
+        })
+        .then(account => registry.setMyName(n_name, account))
+        .then(txHash => {
+            // Show the UI that transaction is processing
+            return web3.eth.getTransactionReceiptMined(txHash);
+        })
+        .catch(error => {
+            console.error(error);
+            // Show the UI that there is a problem with setting the name.
+            // Did you check that the name is taken or not?
+        })
+        .then(receipt => {
+            // Show the UI that it went through
+        });
     }
 }
 
 function updateUi() {
-    var s_account = "ACCOUNT";
-    $("#lbl_account").html(s_account);
+    return web3.eth.getFirstAccountPromise()
+        .then(account => {
+            $("#lbl_account").html(account);
+            return registry.getNameOf(account);
+        })
+        .catch(error => {
+            console.error(error);
+            // Show the UI that there is no account. No need to alert.
+        })
+        .then(name => {
+            // Show the UI the current name
+        })
+        .catch(error => {
+            console.error(error);
+            // Show the UI that there is an error to get name.
+        })
 }
 //lbl_account
--- www/app/js/registry.js
+++ www/app/js/registry.js
@@ -1,51 +1,72 @@
 var registry = {
-	filter: null,
+    filter: null,
 
-	/**
-	 * Call this when web3 is ready.
-	 * @returns an empty promise.
-	 */
-	prepare: function(web3) {
-		Registry.setProvider(web3.currentProvider);
-		return web3.net.getVersion()
-			.then(version => {
-				Registry.setNetwork(version);
-			});
-	},
+    /**
+     * Call this when web3 is ready.
+     * @returns an empty promise.
+     */
+    prepare: function(web3) {
+        Registry.setProvider(web3.currentProvider);
+        return web3.net.getVersion()
+            .then(version => {
+                Registry.setNetwork(version);
+            });
+    },
 
-	/**
-	 * Call this to get the name of an address
-	 * returns a promise with the name.
-	 */
-	getNameOf: function(address) {
-		return Registry.deployed()
-			.names(address);
-	},
+    /**
+     * Call this to get the name of an address
+     * returns a promise with the name.
+     */
+    getNameOf: function(address) {
+        return Registry.deployed()
+            .names(address);
+    },
 
-	/**
-	 * Call this to start listening for events.
-	 * callback: a function that accepts (error, updateArguments).
-	 * updateArguments are like {
-	 *     some other less important arguments,
-	 *     args: {
-	 *         who: an address,
-	 *         name: a name,
-	 *     }
-	 * }
-	 * @returns nothing.
-	 */
-	listenToUpdates: function(callback) {
-		if (registry.filter == null) {
-			registry.filter = Registry.deployed().LogNameChanged({}, { fromBlock: 0 });
-		}
-		registry.filter.watch(callback);
-	},
+    /**
+     * Call this to get the address behind a name.
+     * returns a promise with the address.
+     */
+    getAddressOf: function(name) {
+        return Registry.deployed()
+            .addresses(name);
+    },
 
-	/**
-	 * Call this to stop listening.
-	 */
-	stopListeningToUpdates: function() {
-		registry.filter.stopWatching();
-		registry.filter = null;
-	}
+    /**
+     * Call this to set the name to the address
+     * returns a promise with the transaction hash.
+     * the transaction is not mined yet.
+     */
+    setNameTo: function(newName, address) {
+        return Registry.setName.sendTransaction(newName, { from: address, gas: 500000 });
+    },
+
+    /**
+     * Call this to start listening for events.
+     * callback: a function that accepts (error, updateArguments).
+     * updateArguments are like {
+     *     some other less important arguments,
+     *     args: {
+     *         who: an address,
+     *         name: a name,
+     *     }
+     * }
+     * @returns nothing.
+     */
+    listenToUpdates: function(callback, byWhat) {
+        if (typeof byWhat == "undefined") {
+            byWhat = {};
+        }
+        if (registry.filter == null) {
+            registry.filter = Registry.deployed().LogNameChanged(byWhat, { fromBlock: 0 });
+        }
+        registry.filter.watch(callback);
+    },
+
+    /**
+     * Call this to stop listening.
+     */
+    stopListeningToUpdates: function() {
+        registry.filter.stopWatching();
+        registry.filter = null;
+    }
 }
