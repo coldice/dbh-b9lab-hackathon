b8e8c70894ad408f47320913cb53bf832a43647e
baptistecolin <baptiste.colin@outlook.com>
2017-02-10 21:11:30 +0100

    adds an assert to the test regarding getNameOf

--- www/test/registry.js
+++ www/test/registry.js
@@ -2,12 +2,13 @@ const chai = require('chai');
 const spies = require('chai-spies');
 chai.use(spies);
 var expect = chai.expect;
-
+var web3, Registry;
 require('../app/js/registry.js');
+
 
 describe("basic calls", function() {
 
-    var web3, Registry;
+    
 
     beforeEach("prepare spies", function() {
         web3 = {
@@ -25,11 +26,22 @@ describe("basic calls", function() {
             setNetwork: function(network) {
                 console.log(network);
                 console.log(typeof network);
+            },
+            deployed: function() {
+            	return {names: function(name) {
+            		var namesArray = {"0x0": "testName"};
+            		return new Promise(function (resolve, reject) {
+            			return resolve(namesArray[name]);
+            		});
+            	}
+           		};	
             }
-        };
+        }
+    
         web3.version.getNetworkPromise = chai.spy(web3.version.getNetworkPromise);
         Registry.setProvider = chai.spy(Registry.setProvider);
         Registry.setNetwork = chai.spy(Registry.setNetwork);
+        Registry.deployed = chai.spy(Registry.deployed);
         expect(web3.version.getNetworkPromise).to.be.spy;
         expect(Registry.setProvider).to.be.spy;
         expect(Registry.setNetwork).to.be.spy;
@@ -40,13 +52,21 @@ describe("basic calls", function() {
             .then(() => {});
         expect(web3.version.getNetworkPromise).to.have.been.called();
         expect(Registry.setProvider).to.have.been.called.with("currentProvider1");
-        expect(Registry.setNetwork).to.have.been.called.with("45"); // This one does not pass
+        expect(Registry.setNetwork).to.have.been.called.with('45'); // This one does not pass
     });
 
     it("getNameOf called sub-functions as expected", function() {
+    	registry.prepare(web3, Registry)
+    		.then(() => {});
+    	registry.getNameOf("0x0")
+    	.then(function(name) {
+    		expect(name).to.equal("testName");
+    	});
+    	expect(Registry.deployed).to.have.been.called.once;
     });
 
     it("getAdressOf called sub-functions as expected", function() {
+
     });
 
     it("setNametTo called sub-functions as expected", function() {
