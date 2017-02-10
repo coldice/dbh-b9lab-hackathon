e522fc6c52b911329fa5b1d658d2f41317a98548
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-02-10 18:39:16 +0100

    Got some test working on UI.

--- www/test/registry.js
+++ www/test/registry.js
@@ -3,29 +3,43 @@ const spies = require('chai-spies');
 chai.use(spies);
 var expect = chai.expect;
 
-const web3 = {
-	net: {
-		getVersion: function() {
-			return new Promise(function() {}
-				)
-		}
-	},
-	currentProvider: {}
-};
-const Registry = {
-	setProvider: function() {},
-	setNetwork: function() {}
-};
-
 require('../app/js/registry.js');
 
 describe("basic calls", function() {
-	console.log(registry);
-	it("prepare called as expected", function() {
-		// console.log(Registry);
-		var spiedWeb3GetVersion = chai.spy(web3.net.getVersion);
-		expect(spiedWeb3GetVersion).to.be.spy;
-		registry.prepare(web3, Registry);
-		expect(spiedWeb3GetVersion).to.have.been.called();
-	});
+
+    var web3, Registry;
+
+    beforeEach("prepare spies", function() {
+        web3 = {
+            net: {
+                getVersionPromise: () => {
+                    return new Promise(function (resolve, reject) {
+                        return resolve("45")
+                    });
+                },
+            },
+            currentProvider: "currentProvider1"
+        };
+        Registry = {
+            setProvider: function(provider) {},
+            setNetwork: function(network) {
+                console.log(network);
+                console.log(typeof network);
+            }
+        };
+        web3.net.getVersionPromise = chai.spy(web3.net.getVersionPromise);
+        Registry.setProvider = chai.spy(Registry.setProvider);
+        Registry.setNetwork = chai.spy(Registry.setNetwork);
+        expect(web3.net.getVersionPromise).to.be.spy;
+        expect(Registry.setProvider).to.be.spy;
+        expect(Registry.setNetwork).to.be.spy;
+    });
+
+    it("prepare called sub-functions as expected", function() {
+        registry.prepare(web3, Registry)
+            .then(() => {});
+        expect(web3.net.getVersionPromise).to.have.been.called();
+        expect(Registry.setProvider).to.have.been.called.with("currentProvider1");
+        expect(Registry.setNetwork).to.have.been.called.with("45"); // This one does not pass
+    });
 });
