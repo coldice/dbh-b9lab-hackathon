8a7274d10e51f4a42c4612ae291838ba9932efc2
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-02-10 19:55:23 +0100

    Added Graph contract.

---
+++ truffle/contracts/Graph.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.4.5;
+
+import "WithConfirmation.sol";
+
+contract Graph is WithConfirmation {
+    struct Link {
+        address to;
+        uint loss; // The unit is such that 1% is encoded as 1000
+        uint throughput; // The unit is microWatt.
+        string location;
+    }
+    
+    /**
+     * from => to
+     */
+    mapping (address => Link) public directedLinks;
+    
+    event LogLinkAdded(
+        address indexed from,
+        address indexed to,
+        uint loss,
+        uint throughput,
+        string location);
+
+    function Graph()
+        WithConfirmation(2) {
+    }
+
+    modifier yourLinkOnly(address from, address to) {
+        // Checking for dummy values first
+        if (from == 0 || to == 0 || !isYourLink(from, to)) {
+            throw;
+        }
+        _;
+    }
+
+    function isYourLink(address from, address to)
+        constant
+        returns (bool isIndeed) {
+        return msg.sender == from
+            || msg.sender == to;
+    }
+
+    function submitLink(address from, address to, uint loss, uint throughput, string location)
+        yourLinkOnly(from, to)
+        isConfirmed
+        returns (bool successful) {
+        directedLinks[from] = Link({
+            to: to,
+            loss: loss,
+            throughput: throughput,
+            location: location
+        });
+        LogLinkAdded(from, to, loss, throughput, location);
+        return true;
+    }
+}
---
+++ truffle/test/graph.js
@@ -0,0 +1,227 @@
+const Extensions = require("../utils/extensions.js");
+Extensions.init(web3, assert);
+
+contract('Graph', function(accounts) {
+
+    var user1, user2, user3, user4;
+
+    before("should prepare accounts", function() {
+        assert.isAtLeast(accounts.length, 4, "should have at least 4 accounts");
+        user1 = accounts[0];
+        user2 = accounts[1];
+        user3 = accounts[2];
+        user4 = accounts[3];
+        return Extensions.makeSureAreUnlocked(
+                [ user1, user2 ])
+            .then(function() {
+                return Extensions.makeSureHasAtLeast(
+                    user1,
+                    [ user2 ],
+                    web3.toWei(1));
+            });
+    });
+
+    describe("basic testing", function() {
+
+        var instance;
+
+        beforeEach("should create a brand new graph", function() {
+            return Graph.new({ from: user1 })
+                .then(created => {
+                    instance = created;
+                });
+        });
+
+        it("should have proper initial values", function() {
+            return instance.requiredCount()
+                .then(requiredCount => {
+                    assert.strictEqual(requiredCount.toNumber(), 2, "should have been in constructor");
+                })
+        });
+
+        it("should test if you are from or to", function() {
+            return Promise.all([
+                    instance.isYourLink(user1, user2, { from: user1 }),
+                    instance.isYourLink(user1, user2, { from: user2 }),
+                    instance.isYourLink(user1, user2, { from: user3 })
+                ])
+                .then(isYourLinks => {
+                    assert.isTrue(isYourLinks[0], "user1 should be");
+                    assert.isTrue(isYourLinks[1], "user2 should be");
+                    assert.isFalse(isYourLinks[2], "user3 should not be");
+                });
+        });
+
+        it("should fail if you are not from or to", function() {
+            return Extensions.expectedExceptionPromise(
+                () => instance.submitLink(user1, user2, 1000, 1000000, "somewhere", { from: user3, gas: 3000000 }),
+                3000000);
+        });
+
+        it("should fail if from is 0", function() {
+            return Extensions.expectedExceptionPromise(
+                () => instance.submitLink(0, user2, 1000, 1000000, "somewhere", { from: user2, gas: 3000000 }),
+                3000000);
+        });
+
+        it("should fail if to is 0", function() {
+            return Extensions.expectedExceptionPromise(
+                () => instance.submitLink(user1, 0, 1000, 1000000, "somewhere", { from: user1, gas: 3000000 }),
+                3000000);
+        });
+
+        it("should wait for a confirmation when submitting a link as from", function() {
+            var callData = instance.contract.submitLink.getData(user1, user2, 1000, 1000000, "somewhere");
+            return instance.submitLink.call(user1, user2, 1000, 1000000, "somewhere", { from: user1 })
+                .then(successful => {
+                    assert.isFalse(successful, "should not pass because it is the first");
+                    return instance.submitLink(user1, user2, 1000, 1000000, "somewhere", { from: user1 });
+                })
+                .then(txHash => Promise.all([
+                        web3.eth.getTransactionReceiptMined(txHash),
+                        instance.calculateKey(callData)
+                    ]))
+                .then(receiptAndKey => {
+                    var receivedEvent = instance.OnConfirmationRequired().formatter(receiptAndKey[0].logs[0]);
+                    var key = receiptAndKey[1];
+                    assert.strictEqual(receivedEvent.args.key, key, "should be the key");
+                    return Promise.all([
+                             instance.directedLinks(user1),
+                             instance.confirmations(key),
+                             instance.getConfirmationOf(key, user1),
+                             instance.getConfirmationOf(key, user2)
+                        ]);
+                })
+                .then(infos => {
+                    assert.strictEqual(infos[0][0], "0x0000000000000000000000000000000000000000", "should not set link");
+                    assert.strictEqual(infos[1].toNumber(), 1, "should have 1 confirmed count");
+                    assert.isTrue(infos[2], "should be confirmed");
+                    assert.isFalse(infos[3], "should not be confirmed");
+                });
+        });
+
+        it("should wait for a confirmation when submitting a link as to", function() {
+            var callData = instance.contract.submitLink.getData(user1, user2, 1000, 1000000, "somewhere");
+            return instance.submitLink.call(user1, user2, 1000, 1000000, "somewhere", { from: user2 })
+                .then(successful => {
+                    assert.isFalse(successful, "should not pass because it is the first");
+                    return instance.submitLink(user1, user2, 1000, 1000000, "somewhere", { from: user2 });
+                })
+                .then(txHash => Promise.all([
+                        web3.eth.getTransactionReceiptMined(txHash),
+                        instance.calculateKey(callData)
+                    ]))
+                .then(receiptAndKey => {
+                    var receivedEvent = instance.OnConfirmationRequired().formatter(receiptAndKey[0].logs[0]);
+                    var key = receiptAndKey[1];
+                    assert.strictEqual(receivedEvent.args.key, key, "should be the key");
+                    return Promise.all([
+                             instance.directedLinks(user1),
+                             instance.confirmations(key),
+                             instance.getConfirmationOf(key, user1),
+                             instance.getConfirmationOf(key, user2)
+                        ]);
+                })
+                .then(infos => {
+                    assert.strictEqual(infos[0][0], "0x0000000000000000000000000000000000000000", "should not set link");
+                    assert.strictEqual(infos[1].toNumber(), 1, "should have 1 confirmed count");
+                    assert.isFalse(infos[2], "should not be confirmed");
+                    assert.isTrue(infos[3], "should be confirmed");
+                });
+        });
+
+    });
+
+    describe("create link start with from", function() {
+
+        var instance, key1;
+
+        beforeEach("should create a brand new graph with a pending link", function() {
+            return Graph.new({ from: user1 })
+                .then(created => {
+                    instance = created;
+                    return instance.submitLink(user1, user2, 1000, 1000000, "somewhere", { from: user1 });
+                })
+                .then(txHash => web3.eth.getTransactionReceiptMined(txHash))
+                .then(receipt => {
+                    var receivedEvent = instance.OnConfirmationRequired().formatter(receipt.logs[0]);
+                    key1 = receivedEvent.args.key;
+                });
+        });
+
+        it("should fail if submit with user1 again", function() {
+            return Extensions.expectedExceptionPromise(
+                () => instance.submitLink(user1, user2, 1000, 1000000, "somewhere", { from: user1, gas: 3000000 }),
+                3000000);
+        });
+
+        it("should create link with user2", function() {
+            return instance.submitLink.call(user1, user2, 1000, 1000000, "somewhere", { from: user2 })
+                .then(successful => {
+                    assert.isTrue(successful, "should be possible to add link");
+                    return instance.submitLink(user1, user2, 1000, 1000000, "somewhere", { from: user2 });
+                })
+                .then(txHash => web3.eth.getTransactionReceiptMined(txHash))
+                .then(receipt => {
+                    var receivedEvent = instance.LogLinkAdded().formatter(receipt.logs[0]);
+                    assert.strictEqual(receivedEvent.args.from, user1, "should be the from");
+                    assert.strictEqual(receivedEvent.args.to, user2, "should be the to");
+                    assert.strictEqual(receivedEvent.args.loss.toNumber(), 1000, "should be the loss");
+                    assert.strictEqual(receivedEvent.args.throughput.toNumber(), 1000000, "should be the throughput");
+                    assert.strictEqual(receivedEvent.args.location, "somewhere", "should be the location");
+                    return instance.getConfirmationOf(key1, user2);
+                })
+                .then(isConfirmed => {
+                    assert.isTrue(isConfirmed, "should have been marked");
+                })
+        });
+
+    });
+
+    describe("create link start with to", function() {
+
+        var instance, key1;
+
+        beforeEach("should create a brand new graph with a pending link", function() {
+            return Graph.new({ from: user1 })
+                .then(created => {
+                    instance = created;
+                    return instance.submitLink(user1, user2, 1000, 1000000, "somewhere", { from: user2 });
+                })
+                .then(txHash => web3.eth.getTransactionReceiptMined(txHash))
+                .then(receipt => {
+                    var receivedEvent = instance.OnConfirmationRequired().formatter(receipt.logs[0]);
+                    key1 = receivedEvent.args.key;
+                });
+        });
+
+        it("should fail if submit with user2 again", function() {
+            return Extensions.expectedExceptionPromise(
+                () => instance.submitLink(user1, user2, 1000, 1000000, "somewhere", { from: user2, gas: 3000000 }),
+                3000000);
+        });
+
+        it("should create link with user1", function() {
+            return instance.submitLink.call(user1, user2, 1000, 1000000, "somewhere", { from: user1 })
+                .then(successful => {
+                    assert.isTrue(successful, "should be possible to add link");
+                    return instance.submitLink(user1, user2, 1000, 1000000, "somewhere", { from: user1 });
+                })
+                .then(txHash => web3.eth.getTransactionReceiptMined(txHash))
+                .then(receipt => {
+                    var receivedEvent = instance.LogLinkAdded().formatter(receipt.logs[0]);
+                    assert.strictEqual(receivedEvent.args.from, user1, "should be the from");
+                    assert.strictEqual(receivedEvent.args.to, user2, "should be the to");
+                    assert.strictEqual(receivedEvent.args.loss.toNumber(), 1000, "should be the loss");
+                    assert.strictEqual(receivedEvent.args.throughput.toNumber(), 1000000, "should be the throughput");
+                    assert.strictEqual(receivedEvent.args.location, "somewhere", "should be the location");
+                    return instance.getConfirmationOf(key1, user1);
+                })
+                .then(isConfirmed => {
+                    assert.isTrue(isConfirmed, "should have been marked");
+                })
+        });
+
+    });
+
+});
