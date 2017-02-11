04c9dbc695153e0e5562e3047e09e41df7f721ec
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-02-11 16:36:50 +0100

    Added empty functions to be populated for list-available.

--- www/app/js/add.js
+++ www/app/js/add.js
@@ -19,5 +19,31 @@ function updateUi() {
 //lbl_account
 //txt_address
 
+/**
+ * Returns an empty <tr> that is not yet added to the DOM.
+ * It will represendt an end-point in list-available.html.
+ * All fields are ready to be populated.
+ */
+function createEmptyEndpointRow() {
+}
+
+/**
+ * Populates a <tr> object with the info associated.
+ * endpointInfo is in the form of: {
+ *     who: hex string,
+ *     name: string,
+ *     pointType: number,
+ *     location: string   
+ * }
+ */
+function populateEndpointRow(trObject, endpointInfo) {
+}
+
+/**
+ * Finds the pertinent row in tableObject, and passes it on to populateEndpointRow.
+ */
+function findAndPopulateEndpointRow(tableObjet, endpointInfo) {
+}
+
 // add an appropriate event listener
 window.addEventListener("web3Ready", updateUi);
