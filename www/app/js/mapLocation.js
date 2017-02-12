cd4ecee5a7a000d75328347ffbd11b84b47289ae
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-02-12 05:54:39 +0100

    Added more to the map markers.

--- www/app/js/mapLocation.js
+++ www/app/js/mapLocation.js
@@ -1,5 +1,8 @@
 function updateUi() {
     var markers = {}; // Markers are kept by address
+    var infowindow = new google.maps.InfoWindow({
+        content: ""
+    });
     registry.listenToUpdates((error, infoChanged) => {
         console.log(infoChanged);
         if (error) {
@@ -15,14 +18,20 @@ function updateUi() {
                     marker = new google.maps.Marker({
                         position: position,
                         map: map,
-                        title: infoChanged.args.name
+                        title: infoChanged.args.name,
+                        label: infoChanged.args.pointType + ""
                     });
                     markers[infoChanged.args.who] = marker;
                 } else {
                     console.log("update");
                     marker.setPosition(new google.maps.LatLng(position.lat, position.lng));
                     marker.setTitle(infoChanged.args.name);
+                    marker.setLabel(infoChanged.args.pointType + "");
                 }
+                marker.addListener('click', function() {
+                    infowindow.setContent(infoChanged.args.name);
+                    infowindow.open(map, marker);
+                });
             } catch(error) {
                 console.log("Cannot parse", infoChanged.args.location);
             }
